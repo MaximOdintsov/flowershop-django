f5278a400dc130a688f140d2a4dedc26a6582bbf
Maxim <flipsidebeat@gmail.com>
2023-01-14 16:03:12 +0300

    0.5.2.7

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -187,20 +187,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-#
-# # for prod
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = BASE_DIR / "media"
 
-# for dev
+# for prod
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/image/"
+
+# # for dev
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = "/image/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
