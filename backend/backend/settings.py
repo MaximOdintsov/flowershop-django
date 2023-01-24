f15a77998c5751dbe3647504aba6c49ec5611eea
Maxim <flipsidebeat@gmail.com>
2023-01-24 23:26:23 +0300

    0.5.16 - changed icons and templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -172,9 +172,7 @@ if int(os.getenv('PRODUCTION_VERSION')):
 else:
     # static and media for dev
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static',
-    ]
+    STATIC_ROOT = BASE_DIR / 'static'
     MEDIA_ROOT = BASE_DIR / 'media'
     MEDIA_URL = '/image/'
 
